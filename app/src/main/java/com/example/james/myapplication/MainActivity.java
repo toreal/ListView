1b8b3deb9613289e002d437083612abcf3e2c903
James Wu <toreal@gmail.com>
2017-11-16 18:14:36 +0800

    item click

--- app/src/main/java/com/example/james/myapplication/MainActivity.java
+++ app/src/main/java/com/example/james/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.james.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
         String [] items={"item1","item2","James","Joyce","John"};
         ArrayAdapter<String> adapter= new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1, items);
         myview.setAdapter(adapter);
-        
+
+        myview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = ((TextView)view).getText().toString();
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
 
 
     }
